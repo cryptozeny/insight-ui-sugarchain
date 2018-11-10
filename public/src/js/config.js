ed5e7c7acfb7a0d84a5d61a2204c6e344d697a00
cryptozeny <cryptozeny@gmail.com>
2018-11-11 01:44:50 +0900

    fix

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'BitZeny Block '
+      title: 'sugarchain Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'BitZeny Transaction '
+      title: 'sugarchain Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'BitZeny Blocks solved Today'
+      title: 'sugarchain Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'BitZeny Blocks solved '
+      title: 'sugarchain Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'BitZeny Address '
+      title: 'sugarchain Address '
     }).
     when('/status', {
       templateUrl: 'views/status.html',
